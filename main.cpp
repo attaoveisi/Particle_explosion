61f56fbc46524687c59927e75e13fb5d5a91850a
ATTA OVEISI <atta.oveisi@torc.ai>
2020-11-12 08:42:22 +0100

    added the sreen

--- main.cpp
+++ main.cpp
@@ -11,9 +11,9 @@ using namespace std;
 using namespace Simulation;
 
 int main() {
-    srand(time(NULL));
+    //srand(time(NULL));
 
-    Swarm swarm;
+    //Swarm swarm;
 
     const int SCREEN_WIDTH = 800;
     const int SCREEN_HIGHT = 600;
@@ -21,13 +21,14 @@ int main() {
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0){
         cout << "SDL init failed!" << endl;
+        return 1;
     }else{
         cout << "Success" << endl;
     }
 
     SDL_Window *window = SDL_CreateWindow("Particle explosion",
                                           SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,
-                                          SCREEN_WIDTH,SCREEN_HIGHT, SDL_WINDOWEVENT_SHOWN);
+                                          SCREEN_WIDTH,SCREEN_HIGHT, SDL_WINDOW_SHOWN);
     if (window == NULL){
         printf("Error by creating window: %s/n", SDL_GetError());
         SDL_Quit();
@@ -35,6 +36,33 @@ int main() {
     }
 
     SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+    SDL_Texture * texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
+                                              SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HIGHT);
+
+    if(renderer == NULL){
+        cout << "Could not create renderer!" << endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 3;
+    }
+    if(texture == NULL){
+        cout << "Could not create texture!" << endl;
+        SDL_DestroyWindow(window);
+        SDL_DestroyRenderer(renderer);
+        SDL_Quit();
+        return 4;
+    }
+
+    Uint32 * buffer = new Uint32[SCREEN_WIDTH*SCREEN_HIGHT];
+
+    memset(buffer, 0, SCREEN_WIDTH*SCREEN_HIGHT*sizeof(Uint32));
+
+    buffer[30000] = 0xFFFFFFFF;
+
+    SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH*sizeof(Uint32));
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
 
     //quit stops the animation
     bool quit = false;
@@ -53,9 +81,11 @@ int main() {
 
     }
 
+    delete [] buffer;
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyTexture(texture);
     SDL_DestroyWindow(window);
-
-
+    SDL_Quit();
 
     cout << "Enjoy!" << endl;
     return 0;
